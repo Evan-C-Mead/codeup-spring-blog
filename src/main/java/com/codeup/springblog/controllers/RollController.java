aabb8185ec099643b7b174cc05794fd55f5c808f
Evan Mead <evan.c.mead@gmail.com>
2021-02-10 11:56:18 -0600

    roll-dice and rolled-dice complete

---
+++ src/main/java/com/codeup/springblog/controllers/RollController.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RollController {
+
+    @GetMapping("/roll-dice")
+    public String rollView() {
+        return "roll";
+    }
+
+    @GetMapping("/roll-dice/{number}")
+    public String rolledDiceView(@PathVariable int number, Model model) {
+        int random = (int) Math.ceil(Math.random() * 6);
+        String message;
+
+        if (random == number) {
+            message = "You guessed the roll correctly!";
+        } else {
+            message = "Sorry, try again.";
+        }
+
+        model.addAttribute("message", message);
+        model.addAttribute("number", number);
+        model.addAttribute("random", random);
+        return "rolled-dice";
+    }
+
+}
